24f18d249a99ab0b05c10e09f8eac3d5c0d8163f
Jijo K Mathew <60013314+jkmathew07@users.noreply.github.com>
2026-08-06 09:39:52 +0100

    updated_play

--- play.sh
+++ play.sh
@@ -1,6 +1,8 @@
  ansible-playbook playbook.yml -i inventory/hosts.yml \
    -e "target_host=genx-db-711.genex.com scale_option=scale_up \
-        db_sid=PRODCDB  sga_max_size_gb=4 \
+        db_sid=PRODCDB \
+        db_home=/u01/app/oracle/product/19.3.0/dbhome_1 \
+        sga_max_size_gb=4 \
         sga_target_gb=6 pga_aggregate_target_gb=2 pga_aggregate_limit_gb=4 \
         processes=1000 sessions=1522 target_server_ram_gb=16" 
 #     --vault-password-file=.vaultpass
