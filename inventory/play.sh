87ee0ca63cc1f11bb6b645ec7d44dca54f2743bb
Jijo K Mathew <60013314+jkmathew07@users.noreply.github.com>
2026-08-06 07:15:11 +0100

    updated_db_id

---
+++ inventory/play.sh
@@ -0,0 +1,8 @@
+ ansible-playbook playbook.yml -i inventory/hosts.yml \
+   -e "target_host=genx-db-711.genex.com scale_option=scale_up \
+        db_sid=PRODCDB  sga_max_size_gb=8 \
+        sga_target_gb=10 pga_aggregate_target_gb=2 pga_aggregate_limit_gb=4 \
+        processes=1000 sessions=1500 target_server_ram_gb=16" 
+#     --vault-password-file=.vaultpass
+
+#formula for sessions = SESSIONS = (1.5 * PROCESSES) + 22
